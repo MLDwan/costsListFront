0be49b81b5579cff17ddabac165382c99ed64438
Ivan Petrenko <ivanpetrenko.exceedteam@gmail.com>
2022-02-18 16:19:24 +0300

    add function change

--- script.js
+++ script.js
@@ -7,16 +7,15 @@ window.onload = async() => {
   const addInList = document.getElementById("addInList");
   addInList.onclick = () => addCosts();
   const inputPlace = document.getElementById("inputPlace");
-  inputPlace.addEventListener("change", inputValue);
+  inputPlace.addEventListener("change", inputPlaceValue);
   const inputSpent = document.getElementById("inputSpent");
-  inputSpent.addEventListener("change", inputValue);
+  inputSpent.addEventListener("change", inputSpentValue);
     
-  
   const resp = await fetch("http://localhost:8000/allCosts", {
     method: "GET",
   });
   let result = await resp.json();
-  listCosts = result.body;
+  listCosts = result.data;
 
   render();
 };
@@ -24,6 +23,11 @@ window.onload = async() => {
 const addCosts = async() => {
   let placeName = inputPlace.value;
   let spent = inputSpent.value;
+
+  if (inputPlace.value === "" || inputSpent.value === "") {
+    alert("Заполните все поля");
+  };
+
   const resp = await fetch("http://localhost:8000/createCosts", {
     method: "POST",
     headers: {
@@ -31,18 +35,15 @@ const addCosts = async() => {
       "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({
-      place: inputPlace.value,
-      date: Date(),
-      spent: inputSpent.value
+      place: placeName,
+      date: new Date(),
+      spent: spent
     }),
   });
-  if (inputPlace.value === "" || inputSpent.value === "") {
-    alert("Заполните все поля");
-  };
-
-  let costs = { place: placeName, spent: spent };
-  listCosts.push(costs);
-
+  
+  const result  = await resp.json();
+  listCosts = result.data;
+  
   inputPlace.value = "";
   inputSpent.value = "";
   place = "";
@@ -51,100 +52,115 @@ const addCosts = async() => {
   render();
 };
 
-let inputValue = (event) => {
-  inputValue = event.target.value;
-};
+
+
 
 const render = () => {
-  
   sum = 0;
   const content = document.getElementById("content");
+  
   while (content.firstChild) {
     content.removeChild(content.lastChild);
   };
+
   let summ = document.createElement("p");
   content.appendChild(summ);
   
   listCosts.map((item, index) => {
-    
-    const id = listCosts[index]._id;
-    const placeIndex = listCosts[index].place;
-    const spentIndex = listCosts[index].spent;
-    const newSpent = Number(item.spent);
+    let {place, spent, _id, flag} = listCosts[index];
+    const newSpent = Number(spent);
     const container = document.createElement("div");
     container.id = `costs-${index}`;
     container.className = "costs-container";
+
     if(listCosts[index].flag === 1) {
-      placeInput = document.createElement("input");
-      placeInput.type = "text";
-      placeInput.addEventListener("change", inputValue);
-      placeInput.value = listCosts[index].place;
-      container.appendChild(placeInput);
+      inputPlaceChange = document.createElement("input");
+      inputPlaceChange.type = "text";
+      inputPlaceChange.addEventListener("change", inputChangePlaceValue);
+      inputPlaceChange.value = place;
+      container.appendChild(inputPlaceChange);
       
-      spentInput = document.createElement("input");
-      spentInput.type = "text";
-      spentInput.addEventListener("change", inputValue);
-      spentInput.value = listCosts[index].spent;
-      container.appendChild(spentInput);
+      inputSpentChange = document.createElement("input");
+      inputSpentChange.type = "text";
+      inputSpentChange.addEventListener("change", inputChangeSpentValue);
+      inputSpentChange.value = listCosts[index].spent;
+      container.appendChild(inputSpentChange);
 
       const acceptButton = document.createElement("button");
-      acceptButton.onclick = () => acceptFun(placeIndex, spentIndex);
+      acceptButton.onclick = () => acceptFun(place, spent, _id, index);
       container.appendChild(acceptButton);
 
       const cancelButton = document.createElement("button");
       cancelButton.onclick = () => cancelFun(index);
       container.appendChild(cancelButton);
 
-    const imageAccept = document.createElement("img");
-    imageAccept.src = "img/accept.svg";
-    acceptButton.appendChild(imageAccept);
+      const imageAccept = document.createElement("img");
+      imageAccept.src = "img/accept.svg";
+      acceptButton.appendChild(imageAccept);
 
-    const imageCancel = document.createElement("img");
-    imageCancel.src = "img/cancel.svg";
-    cancelButton.appendChild(imageCancel);
-    } else {let text = document.createElement("p");
-    text.innerText = `${index + 1}) Магазин: "${item.place}"`;
-
-    let date = document.createElement('p');
-    date.innerText = new Date().toLocaleDateString();
-
-    let spent = document.createElement("p");
-    spent.innerText = listCosts[index].spent;
+      const imageCancel = document.createElement("img");
+      imageCancel.src = "img/cancel.svg";
+      cancelButton.appendChild(imageCancel);
+    } else {
+      let text = document.createElement("p");
+      text.innerText = `${index + 1}) Магазин: "${item.place}"`;
+      
+      let date = document.createElement('p');
+      date.innerText = new Date().toLocaleDateString();
+      
+      let spent = document.createElement("p");
+      spent.innerText = listCosts[index].spent;
+      
+      sum = sum + newSpent;
+      summ.innerText = `Итого: ${sum} Р.`;
+      
+      const buttonArea = document.createElement("div");
+      buttonArea.className = "buttonArea";
+      
+      const changeButton = document.createElement("button");
+      changeButton.onclick = () => changeFun(index);
+      
+      const imageEdit = document.createElement("img");
+      imageEdit.src = "img/editor.svg";
+      changeButton.appendChild(imageEdit);
+      
+      const deleteButton = document.createElement("button");
+      deleteButton.onclick = () => deleteFun(_id);
+      
+      const imageDelete = document.createElement("img");
+      imageDelete.src = "img/remove.svg";
+      deleteButton.appendChild(imageDelete);
+      
+      buttonArea.appendChild(changeButton);
+      buttonArea.appendChild(deleteButton);
+      container.appendChild(text);
+      container.appendChild(date);
+      container.appendChild(spent);
+      container.appendChild(buttonArea);
+    };
     
-    sum = sum + newSpent;
-    summ.innerText = `Итого: ${sum} Р.`;
-
-    const buttonArea = document.createElement("div");
-    buttonArea.className = "buttonArea";
-
-    const changeButton = document.createElement("button");
-    changeButton.onclick = () => changeFun(index);
-
-    const imageEdit = document.createElement("img");
-    imageEdit.src = "img/editor.svg";
-    changeButton.appendChild(imageEdit);
-
-    const deleteButton = document.createElement("button");
-    deleteButton.onclick = () => deleteFun(id);
-
-    const imageDelete = document.createElement("img");
-    imageDelete.src = "img/remove.svg";
-    deleteButton.appendChild(imageDelete);
-
-    buttonArea.appendChild(changeButton);
-    buttonArea.appendChild(deleteButton);
-    container.appendChild(text);
-    container.appendChild(date);
-    container.appendChild(spent);
-    container.appendChild(buttonArea);
-    }
-
     content.appendChild(container);
   });
 };
 
-const deleteFun = async (id) => {
-  const resp = await fetch(`http://localhost:8000/deleteCosts?_id=${id}`, {
+let inputPlaceValue = (event) => {
+  inputPlace.value = event.target.value;
+};
+
+let inputSpentValue = (event) => {
+  inputSpent.value = event.target.value;
+};
+
+let inputChangePlaceValue = (event) => {
+  inputPlaceChange.value = event.target.value;
+};
+
+let inputChangeSpentValue = (event) => {
+  inputSpentChange.value = event.target.value;
+};
+
+const deleteFun = async (_id) => {
+  const resp = await fetch(`http://localhost:8000/deleteCosts?_id=${_id}`, {
     method: "DELETE",
   });
   let result = await resp.json();
@@ -155,7 +171,6 @@ const deleteFun = async (id) => {
 
 const changeFun =  (index) => {
   listCosts[index].flag = 1;
-
   render();
 };
 
@@ -163,12 +178,11 @@ const cancelFun = (index) => {
   listCosts[index].flag = 0;
 
   render();
-}
-const acceptFun = async (placeIndex, spentIndex) => { 
-  console.log(inputPlace, inputSpent);
-  inputPlace = inputValue;
-  inputSpent = inputValue;
-  console.log(inputPlace, inputSpent);
+};
+
+const acceptFun = async (place, spent, _id, index) => { 
+  place = inputPlaceChange.value;
+  spent = inputSpentChange.value;
   const resp = await fetch("http://localhost:8000/changeCost", {
     method: "PATCH",
     headers: {
@@ -176,17 +190,17 @@ const acceptFun = async (placeIndex, spentIndex) => {
       "Access-Control-Allow-Origin": "*",
     },
     body: JSON.stringify({
-      place: inputPlace.value,
-      date: Date(),
-      spent: inputSpent.value
+      place: place,
+      date: new Date(),
+      spent: spent,
+      _id: _id
     }),
   });
 
   let result = await resp.json();
   listCosts = result.data;
-
-  place = "";
-  spent = "";
+  listCosts[index].flag = 0;
+  
   inputSpent.value = "";
   inputPlace.value = "";
 
